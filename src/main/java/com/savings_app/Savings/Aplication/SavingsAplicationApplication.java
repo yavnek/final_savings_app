1e1b9286d752446f5fdd4199db9839cd412029af
yavnek <jonatan.gasiorowski@gmail.com>
2024-08-12 20:34:55 +0200

    Initial test

---
+++ src/main/java/com/savings_app/Savings/Aplication/SavingsAplicationApplication.java
@@ -0,0 +1,13 @@
+package com.savings_app.Savings.Aplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavingsAplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavingsAplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/savings_app/Savings/Aplication/SavingsAplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.savings_app.Savings.Aplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavingsAplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
